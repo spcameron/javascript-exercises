9d65ad9b84a0b4956f23361d3cbd93cd0750ed7c
spcameron <cameron.seanpatrick@gmail.com>
2025-05-24 10:30:29 -0400

    Complete exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+  newStr = "";
+  for (let i = 0; i < num; i++) {
+    newStr += str;
+  }
+  return newStr;
 };
 
 // Do not edit below this line

83c01d0d023ec74a6480215d1a36846bb078dd4b
spcameron <cameron.seanpatrick@gmail.com>
2025-05-24 15:01:51 -0400

    Complete exercise 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,27 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let isLeapYear;
 
+  if (year % 4 === 0) {
+    if (year % 100 === 0 && year % 400 !== 0) {
+      isLeapYear = false;
+    } else {
+      isLeapYear = true;
+    }
+  } else {
+    isLeapYear = false;
+  }
+
+  return isLeapYear;
+};
+
+// given solution's logic
+
+const leapYears = function (year) {
+  if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line

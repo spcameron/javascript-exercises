16f16951abd19d68627434d3a444951c98f3ec1b
spcameron <cameron.seanpatrick@gmail.com>
2025-05-24 14:50:24 -0400

    Complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (
+    typeof a !== "number" ||
+    typeof b !== "number" ||
+    a < 0 ||
+    b < 0 ||
+    a - Math.floor(a) !== 0 ||
+    b - Math.floor(b) !== 0
+  ) {
+    return "ERROR";
+  }
 
+  let start, end;
+
+  if (a < b) {
+    start = a;
+    end = b;
+  } else {
+    start = b;
+    end = a;
+  }
+
+  let totalSum = 0;
+  for (let i = start; i <= end; i++) {
+    totalSum += i;
+  }
+  return totalSum;
 };
 
 // Do not edit below this line

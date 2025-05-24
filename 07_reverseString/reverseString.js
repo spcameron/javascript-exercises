10d24887ae74895cd4a2aa3e12df0025ce9a2ac9
spcameron <cameron.seanpatrick@gmail.com>
2025-05-24 11:08:49 -0400

    Complete exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  // one-liner
+  return str.split("").reverse().join("");
 
+  // manually with loop
+  arr = str.split("");
+  newStr = "";
+  for (let i = arr.length - 1; i >= 0; i--) {
+    newStr += arr[i];
+  }
+  return newStr;
 };
 
 // Do not edit below this line

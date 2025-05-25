541c4989e8714c618df9d3be17f7141395081b83
spcameron <cameron.seanpatrick@gmail.com>
2025-05-25 06:09:27 -0400

    Complete exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const celsius = (fahrenheit - 32) * (5 / 9);
+  return oneDecimalPlace(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const fahrenheit = celsius * (9 / 5) + 32;
+  return oneDecimalPlace(fahrenheit);
+};
+
+const oneDecimalPlace = function (value) {
+  return Math.round(value * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
